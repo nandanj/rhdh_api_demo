5c86d45659139fe235a1f3d1a8207f224567b3fd
Nandan Joshi <njoshi@redhat.com>
2025-06-30 11:01:48 -0700

    updated to specify values for missing params

--- create_templ8_list_tasks.sh
+++ create_templ8_list_tasks.sh
@@ -1,7 +1,11 @@
+
 # API_BASE_URL = "https://route-backstage-internal-backstage.apps.cluster-h8p9j.h8p9j.sandbox1517.opentlc.com" # <<< IMPORTANT: Replace with your actual API base URL
 # BEARER_TOKEN = "sha256~jMzs7HdcWd_IAmh5FasJsLHGbnnJFu6Ky8ldjvxWlbI" # <<< IMPORTANT: Replace with your actual Bearer Token
-API_BASE_URL="https://backstage-backstage.apps.cluster-tvwbg.tvwbg.sandbox1943.opentlc.com" 
+API_BASE_URL="https://backstage-backstage.apps.cluster-tvwbg.tvwbg.sandbox1943.opentlc.com"
+#API_BASE_URL="https://backstage-backstage.apps.cluster-hwq26.hwq26.sandbox558.opentlc.com"
 BEARER_TOKEN="foobarbaz"
+#HOST="gitlab-gitlab.apps.cluster-hwq26.hwq26.sandbox558.opentlc.com"
+HOST="gitlab-gitlab.apps.cluster-tvwbg.tvwbg.sandbox1943.opentlc.com"
 
 
 # curl -k -X POST "$API_BASE_URL/api/scaffolder/v2/tasks" \
@@ -31,19 +35,27 @@ curl -k -X POST "$API_BASE_URL/api/scaffolder/v2/tasks" \
   -d '{
         "templateRef": "template:default/quarkus-web-template-api-compat-gitlab",
         "values": {
-          "component_id": "wf-q-comp",
+          "component_id": "my-gamma-test",
+          "description": "simple Quarkus app",
+          "group_id": "qgroup",
+          "artifact_id": "qartifact",
           "java_package_name": "com.redhat.rhdh",
-          "image_registry": "OpenShift",
-          "destination": "dev1/wf-q_comp",
+          "image_registry": "Quay",
+          "image_host": "quay.apps.cluster-tvwbg.tvwbg.sandbox1943.opentlc.com",
+          "image_tag": "latest",
+          "destination": "dev1/wells-demo",
           "repo": {
-            "host": "gitlab-gitlab.apps.cluster-tvwbg.tvwbg.sandbox1943.opentlc.com",
+            "host": "'"$HOST"'",
             "owner": "rhdh"
           },
           "username": "dev1"
+        },
+        "secrets": {
+          "image_password": "MTY1MjU0"
         }
       }'
 
 # get all tasks
-# curl -k -X GET "$API_BASE_URL/api/scaffolder/v2/tasks" \
-#   -H "Content-Type: application/json" \
-#   -H "Authorization: Bearer $BEARER_TOKEN"
+ # curl -k -X GET "$API_BASE_URL/api/scaffolder/v2/tasks" \
+ #   -H "Content-Type: application/json" \
+ #   -H "Authorization: Bearer $BEARER_TOKEN"
